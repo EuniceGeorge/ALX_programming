fedcede7f57f75bd414ec93969435f2de8a3fe25
EuniceGeorge <georgeunie408@gmail.com>
2023-03-20 10:51:54 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is postive", n);
+printf(" %d is postive ", n);
 else if (n == 0)
-printf("%d is  zero", n);
+printf(" %d is  zero ", n);
 else
-printf("%d is negative", n);
+printf(" %d is negative ", n);
 return (0);
 }
