ad713207f813f572a4f1f6b621c0e9a30756d5a1
EuniceGeorge <georgeunie408@gmail.com>
2023-03-20 16:05:50 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-if (i != 'q' && i != 'e')
-putchar(i);
-putchar('\n');
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+printf("\n");
 return (0);
 }
 
