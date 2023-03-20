52816b6e5a8d56bc1daac2554959c2e532af050d
EuniceGeorge <georgeunie408@gmail.com>
2023-03-20 16:43:05 -0700

    task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+	putchar(i + '0');
+for (j = 'a'; j <= 'f'; j++)
+	putchar(j);
+putchar ('\n');
+return (0);
+}
