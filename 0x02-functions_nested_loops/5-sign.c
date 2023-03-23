dc6a7aee1b2f31d55b437fa7e774ae0a7916fd28
EuniceGeorge <georgeunie408@gmail.com>
2023-03-22 21:07:00 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: The character to print
+ * Return: 1,0 and -1
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
