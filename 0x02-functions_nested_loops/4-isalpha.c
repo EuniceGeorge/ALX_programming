3ef988e00694219e6ec9815bcfb0245f41b3da7e
EuniceGeorge <georgeunie408@gmail.com>
2023-03-22 20:37:38 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isalpha - Function to print alphabet
+*
+*  @c: The character to print
+*  Return: 1 if it is alphabet
+*  or return  0
+*/
+
+int _isalpha(int c);
+{
+	if ((c >= 97 && c <= 127) || (c >= 65  && c <= 90))
+		return (1);
+	else
+		return (0);
+}
