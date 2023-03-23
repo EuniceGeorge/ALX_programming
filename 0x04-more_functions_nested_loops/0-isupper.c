4ae2706798ccc5694e35f99b70b940f19629f0fa
EuniceGeorge <georgeunie408@gmail.com>
2023-03-23 08:50:02 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - function that returns capital letters
+ *
+ * @c: The character to print
+ * Return: On success 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+		}
+	else
+	{
+		return (0);
+	}
+}
