4b7ddf4d9cc33b80dbfe24c44456d49d70f96ea9
EuniceGeorge <georgeunie408@gmail.com>
2023-03-27 10:31:37 -0700

    swapping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - main entry
+ * @a: first value
+ * @b: Second value
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = 'a';
+	*a = *b;
+
+	*b = c;
+}
