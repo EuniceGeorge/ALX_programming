769227e1fce1eeb30a45b36ada09f41fe55571b2
EuniceGeorge <georgeunie408@gmail.com>
2023-04-09 12:50:32 -0700

    atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,18 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int mul = 1;
-	
+
 	if (argc != 3)
+	{
 	printf("Error\n");
+	return (1);
+	}
 
 	for (i = 1; i < argc; i++)
 
 	{
 		int m = atoi(argv[i]);
+
 		mul = mul * m;
 	}
 	printf("%d\n", mul);
