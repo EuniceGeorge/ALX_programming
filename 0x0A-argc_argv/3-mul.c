765531c72f622a978dd2446c60ba83f60833e2ff
EuniceGeorge <georgeunie408@gmail.com>
2023-04-09 12:41:18 -0700

    atoi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main  - Entry
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 success
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+	
+	if (argc != 3)
+	printf("Error\n");
+
+	for (i = 1; i < argc; i++)
+
+	{
+		int m = atoi(argv[i]);
+		mul = mul * m;
+	}
+	printf("%d\n", mul);
+	return (0);
+}
