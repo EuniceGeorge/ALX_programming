244ad0a41bf87b451ec5c3ff039bc642d1984fc9
EuniceGeorge <georgeunie408@gmail.com>
2023-04-09 09:12:30 -0700

    argc2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main  - Entry
+ *@argc: argument count
+ *@argv: argument vector
+ *Return
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
